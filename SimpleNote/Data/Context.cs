25ab32e74680646bef4b1c9d25c40865ae8e26b6
Cody Tipping <cody.tipping@driveflow.io>
2024-02-02 12:25:25 -0500

    Model Create.

--- SimpleNote/Data/Context.cs
+++ SimpleNote/Data/Context.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace SimpleNote.Data;
 
 public class Context : DbContext
 {
+    public Context(DbContextOptions<Context> options) : base(options) { }
 
+    public DbSet<Note> Notes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
 }
---
+++ SimpleNote/Models/Note.cs
@@ -0,0 +1,7 @@
+﻿namespace SimpleNote.Models;
+
+public class Note
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Content { get; set; } = string.Empty;
+}
