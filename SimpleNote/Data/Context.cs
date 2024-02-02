1bc78edb2a6492466b53504cd1c2dd8c22321e65
Cody Tipping <cody.tipping@driveflow.io>
2024-02-02 12:29:32 -0500

    Instantiated Database. Additional: Cleaned up unnecessary files.

--- SimpleNote/Data/Context.cs
+++ SimpleNote/Data/Context.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using SimpleNote.Models;
 
 namespace SimpleNote.Data;
 
---
+++ SimpleNote/Migrations/20240202172758_DatabaseCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleNote.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatabaseCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
