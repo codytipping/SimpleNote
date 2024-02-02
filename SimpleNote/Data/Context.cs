e6e03868b7f78361c96554a29c8699e9dda14e0d
Cody Tipping <cody.tipping@driveflow.io>
2024-02-02 12:22:50 -0500

    Add project files.

---
+++ SimpleNote/Data/Context.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SimpleNote.Data;
+
+public class Context : DbContext
+{
+
+}
---
+++ SimpleNote/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using SimpleNote.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ContextConnection") ?? throw new InvalidOperationException("Connection string 'ContextConnection' not found.");
+builder.Services.AddDbContext<Context>(options => options.UseSqlServer(connectionString));
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.Run();
